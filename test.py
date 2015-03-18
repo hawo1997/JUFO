6b1cd523c5255d7d396d8412f7c3732efb1dc13c
hawo1997 <hawo1997@t-online.de>
2015-03-18 09:11:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-import math
+import RPi.GPIO as GPIO         #RPi for programming the GPIO header
+import os                       #os for using commands from the system
+import time                     #using time for the sleep function
+
