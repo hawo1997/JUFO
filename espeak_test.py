6908e17d045366a424564a6a71cd3d0c3686d015
samusake <samusake@gmail.com>
2015-05-06 16:02:44 +0200

    Update espeak_test.py

--- espeak_test.py
+++ espeak_test.py
@@ -1,6 +1,7 @@
-# auf rasp müssen espeak, mbrola und mbrola-de5 installiert sein
+# auf rasp müssen espeak,  installiert sein
 import os
 
 def lese_text(t):
-  os.system('espeak -v mb-de5+f1 -s 125 "{0}"'.format(t))
-lese_text("Wer möchte heute mit mir Kuchen backen")
+  os.system('espeak -v de+f2 -s 115 "{0}"'.format(t))
+lese_text("Die Amateurastronomie wird als Hobby von Liebhaberastronomen betrieben, die im Unterschied zu professionellen Astronomen keine beruflichen Interessen mit ihrer Tätigkeit verfolgen. Intensive Naturerlebnisse unter dem Sternhimmel werden dabei mit dem Einsatz sehr unterschiedlicher optischer und astronomischer Instrumente verbunden. Das Spektrum reicht von einfachen Ferngläsern bis zu hoch entwickelten Teleskopen und Kameras und selbst entwickelten elektronischen Steuerungen. Lange Jahre von einer verschwindend kleinen Minderheit von Begeisterten betrieben, hat sich die Amateurastronomie seit Anfang des 20. Jahrhunderts vor allem in den USA zu einem verbreiteten Hobby entwickelt und seit 1950 auch in Europa.")
+
