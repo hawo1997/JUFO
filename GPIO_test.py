864d15b2d8c9b756789a817cf0b165968eea7f64
hawo1997 <hawo1997@t-online.de>
2015-05-06 14:45:49 +0200

    Delete GPIO_test.py

--- GPIO_test.py
+++
@@ -1,4 +0,0 @@
-import RPi.GPIO as GPIO         #RPi for programming the GPIO header
-import os                       #os for using commands from the system
-import time                     #using time for the sleep function
-
