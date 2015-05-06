40376196716437c9a57f64d91816d5ad9998e2ea
samusake <samusake@gmail.com>
2015-05-06 15:01:43 +0200

    Update zugriff_auf_sqlite.py

--- zugriff_auf_sqlite.py
+++ zugriff_auf_sqlite.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3 as lite
+
+
 con = lite.connect('JUFO.db')
 
 with con:
-    
+
     con.row_factory = lite.Row
-       
-    cur = con.cursor() 
-    cur.execute("SELECT * FROM Cars")
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM rasp1")
 
     rows = cur.fetchall()
-
     for row in rows:
-        print "%s %s %s" % (row["Id"], row["Name"], row["Price"])
+        #print "%s %s %s" % (row["Raumnr"], row["Raumname"], row["Oeffnungszeiten"])
