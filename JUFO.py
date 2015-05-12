48bff1e4d51ca379aeafc7bc35585f9c721b43ec
samusake <samusake@gmail.com>
2015-05-12 17:03:14 +0200

    Update JUFO.py

--- JUFO.py
+++ JUFO.py
@@ -1,2 +1,15 @@
+import GPIO.py
+import zugriff_auf_sqlite.py
+import espeak.py
 name=rasp1 #name des Raspeberrys :D
 
+Archiv=False
+Standesamt=False
+
+while True:
+  Archiv=teste_ob_gedrueckt(Archiv)
+  Standesamt=teste_ob_gedrueckt(Standesamt)
+  if Archiv=True:
+    lese_text(auslesen("Archiv"))
+  if Standesamt=True:
+    lese_text(auslesen("Archiv"))
