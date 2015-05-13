bae6a3ff05e972840f3b30f0789e89cf93c79f80
samusake <samusake@gmail.com>
2015-05-13 13:50:52 +0200

    Update JUFO.py

--- JUFO.py
+++ JUFO.py
@@ -7,8 +7,8 @@ r1=False #Raum Nummer 1, hier im Beispiel Archiv
 r2=False #Standesamt
 
 while True:
-  r1=teste_ob_gedrueckt(r1)
-  r2=teste_ob_gedrueckt(r2)
+  r1=teste_ob_gedrueckt("r1")
+  r2=teste_ob_gedrueckt("r2")
   if r1=True:
     lese_text(auslesen(1,name))
   if r2=True:
